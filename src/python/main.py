e6a8771c978962c3c331fc3f9a1e692f57686ed9
Victor <vfahur@gmail.com>
2023-08-24 18:14:07 -0300

    teste2

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-from pathlib import Path
 
 # Inicialização do Pygame
 pygame.init()
@@ -25,10 +24,10 @@ TEXT_SIZE = 70
 GAME_TITLE = "Scope: Mirando Certo"
 
 # Caminho da imagem de fundo
-BACKGROUND_IMAGE_PATH = Path(r"image/fundo_menu.png")
+BACKGROUND_IMAGE_PATH = r"image\fundo_menu.png"  # O caminho da imagem
 
-# Inicialização do Pygame
-pygame.init()
+# Caminho da imagem de fundo para a segunda tela
+SECOND_PAGE_BACKGROUND_PATH = r"image\tela2.png"  # Caminho da nova imagem de fundo
 
 # Criando a tela
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -37,6 +36,7 @@ pygame.display.set_caption("Tela de Início")
 # Carregando a imagem de fundo
 background_image = pygame.image.load(BACKGROUND_IMAGE_PATH).convert()
 background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 # Função para criar o texto com bordas
 def draw_text_with_borders(text, size, color, border_color, x, y):
@@ -74,21 +74,50 @@ def draw_button():
     button_text_rect = button_text_surface.get_rect(center=button_rect.center)
     screen.blit(button_text_surface, button_text_rect)
 
+
+# Função para criar a segunda tela
+def draw_second_page():
+    # Carrega a imagem de fundo para a segunda tela
+    second_page_background = pygame.image.load(SECOND_PAGE_BACKGROUND_PATH).convert()
+    second_page_background = pygame.transform.scale(second_page_background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Desenha a imagem de fundo da segunda tela
+    screen.blit(second_page_background, (0, 0))
+
+
+# Variável de controle para determinar a página
+current_page = "menu"
+
+
+# Função para mudar de página
+def change_page(page):
+    global current_page
+    current_page = page
+
+
 # Loop principal do jogo
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if current_page == "menu":
+                button_rect = pygame.Rect(
+                    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2,
+                    SCREEN_HEIGHT - 180,
+                    BUTTON_WIDTH,
+                    BUTTON_HEIGHT,
+                )
+                if button_rect.collidepoint(event.pos):
+                    change_page("outra_pagina")
 
-    # Desenha a imagem de fundo
     screen.blit(background_image, (0, 0))
 
-    # Desenha o título do jogo no topo da tela com bordas
-    draw_text_with_borders(GAME_TITLE, TEXT_SIZE, TEXT_COLOR, (0, 0, 0), SCREEN_WIDTH // 2, 150)
+    if current_page == "menu":
+        draw_text_with_borders(GAME_TITLE, TEXT_SIZE, TEXT_COLOR, (0, 0, 0), SCREEN_WIDTH // 2, 150)
+        draw_button()
+    elif current_page == "outra_pagina":
+        draw_second_page()  # Desenha a segunda tela
 
-    # Desenha o botão "Start"
-    draw_button()
-
-    # Atualiza a tela
     pygame.display.update()
